6a6e1c78cd0c62724ce057b9a65257eb58944f2a
PearlMarkW <mark@pearl-io.com>
2024-10-08 14:09:50 -0400

    Added favicon for tab image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,8 +12,17 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Your site description here" />
-        <title>Pearl IO</title>
+        <meta name="description" content="Modern Deal Making for Investors" />
+        <title>Wise Strategies</title>
+
+        {/* Link to the favicon */}
+        <link rel="icon" href="/images/wisestrats.ico" />
+
+        {/* You can also include other icon formats for compatibility */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-wisestrats.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/wisestrats-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/wisestrats-16x16.png" />
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
       </Head>
       <div className="antialiased">
         <Header />
